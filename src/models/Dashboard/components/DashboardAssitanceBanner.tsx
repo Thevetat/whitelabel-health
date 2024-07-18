45c650b8bcf9e1bd8e76782db2493966c80db45c
Thevetat <thevetat@proton.me>
2024-07-18 12:25:42 -0500

    📝 (DashboardAssitanceBanner.tsx): update styles in DashboardAssitanceBanner component to improve layout and alignment

--- src/models/Dashboard/components/DashboardAssitanceBanner.tsx
+++ src/models/Dashboard/components/DashboardAssitanceBanner.tsx
@@ -2,8 +2,8 @@ import { Icon } from '@iconify/react';
 
 function DashboardAssistanceBanner() {
     return (
-        <div className="w-full bg-[#EFF4F0] rounded-lg px-6 py-3 flex items-center justify-between border border-[#9AAB7B] text-lg md:text-xl">
-            <div className="flex items-center gap-4 ">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full bg-[#EFF4F0] rounded-lg px-6 py-3 border border-[#9AAB7B] text-lg md:text-xl text-center md:text-start">
+            <div className="flex flex-col md:flex-row items-center gap-4 ">
                 <Icon icon="mingcute:phone-success-line" className="size-8 text-[#9AAB7B]" />
                 <div className="text-[#121212]">Need assitance? Contact your dedicated care guide.</div>
             </div>
