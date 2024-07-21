fdc60982a604e253ff08c3384d00c5e8ff2510fe
Thevetat <thevetat@proton.me>
2024-07-20 21:00:42 -0500

    📝 (Header.tsx): update background color in header component to improve design and readability

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     const { metaTitle } = useNavigation();
 
     return (
-        <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b border-color bg-main-main px-4">
+        <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b border-color bg-[#FAFAFA] px-4">
             <h1 className="text-lg md:text-xl font-semibold">
                 {metaTitle}
             </h1>
